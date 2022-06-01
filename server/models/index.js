a009a4b3ff82e2a09fda94a402123dffeb81c0b4
Getu Taddesse <Getu_Taddesse@mckinsey.com>
2022-06-01 12:37:01 -0400

    Added Models Db connected users and items table created

--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,30 @@ const Sauce = sequelize.define("sauces", {
   image: Sequelize.STRING,
 });
 
+const Item = sequelize.define("items", {
+  name: Sequelize.STRING,
+  price: Sequelize.FLOAT,
+  description: Sequelize.STRING,
+  category: Sequelize.STRING,
+  imageUrl: Sequelize.STRING
+
+});
+
+const User = sequelize.define("users", {
+  firstName: Sequelize.STRING,
+  lastName: Sequelize.STRING,
+  password: Sequelize.STRING,
+  email: Sequelize.STRING
+});
+
+const Order = sequelize.define("orders", {
+  itemName: Sequelize.STRING,
+  description: Sequelize.STRING,
+  quantity: Sequelize.INTEGER,
+  orderDate: Sequelize.DATE
+});
+
 module.exports = {
   db: sequelize,
-  Sauce,
+  Sauce, Item, User, Order
 };
